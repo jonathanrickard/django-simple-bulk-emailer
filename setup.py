22eabec39b8be1e4444ee5cd909e2ace65ac7330
jonathanrickard <jonathan.rickard@gmail.com>
2020-05-05 14:41:50 -0400

    Bumped version number, moved to Python 3.8, requires django-simple-file-handler>=0.2.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django_simple_bulk_emailer',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
@@ -32,7 +32,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
@@ -40,7 +40,7 @@ setup(
         'Django>=2.2.9,<3.0',
         'django-admin-sortable2>=0.6.21',
         'django-ckeditor>=5.6.1',
-        'django-simple-file-handler>=0.2.3',
+        'django-simple-file-handler>=0.2.4',
         'mailchimp3>=3.0',
         'Pillow>=5.0',
         'requests>=2.21.0',
