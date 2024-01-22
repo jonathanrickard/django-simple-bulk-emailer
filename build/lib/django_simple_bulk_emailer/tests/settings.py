47c84e0a4175a88732212105bae43ee0ea2a0720
jonathanrickard <jonathan.rickard@gmail.com>
2024-01-21 19:29:05 -0500

    Added test templates to package

--- build/lib/django_simple_bulk_emailer/tests/settings.py
+++ build/lib/django_simple_bulk_emailer/tests/settings.py
@@ -38,7 +38,9 @@ MIDDLEWARE = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'tests', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
